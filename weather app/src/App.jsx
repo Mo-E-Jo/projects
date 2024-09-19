c859b785f46fe47d092b78db4f4349fc19155aef
Moses Joseph <mosesejoseph@gmail.com>
2024-09-19 07:22:42 -0400

    create vite-react weather base files & started working on the first component (search bar)

---
+++ weather app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './components/search/search'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <Search />
+    </div>
+  )
+}
+
+export default App
